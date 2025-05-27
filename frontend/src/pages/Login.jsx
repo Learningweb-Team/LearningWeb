6219a8f1470785a09f70e9a6591d83d76b208efc
KISHORE-14622 <kishoresanthosh14622@gmail.com>
2025-05-27 19:08:08 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -19,7 +19,7 @@ const Login = () => {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/login", { email, password });
+      const res = await axios.post("https://digital-schools-backend.onrender.com/api/auth/login", { email, password });
 
       if (res.data.token) {
         localStorage.setItem("token", res.data.token);
